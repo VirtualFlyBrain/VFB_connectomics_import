96790eb68100d7cf2a4a3dfe370ca0017b7a20ca
admclachlan <adm71@cam.ac.uk>
2020-12-01 15:59:36 +0000

    Script Update
    -still won't work with catmaid
    -still needs refactoring
    -still needs argpass
    -Does work for neuprint

--- src/VFB_connectomics_import/connectomics_import.py
+++ src/VFB_connectomics_import/connectomics_import.py
@@ -13,35 +13,30 @@ class ConnectomicsImport:
             self.neuprint_client=neuprint.Client(neuprint_endpoint, dataset=neuprint_dataset, token=neuprint_token)
         else: self.neuprint_client=None
 
-    def get_adjacencies(self, accessions, testmode=False):# add testmode
-        # fetch body ids of only neurons with types
-        neur_ids = accessions
+    def get_accessions_from_vfb(self, dataset):
+        accessions = list(map(int, self.vc.neo_query_wrapper.xref_2_vfb_id(db=dataset).keys()))
+        return accessions
+
+    def get_adjacencies_neuprint(self, accessions, testmode=False):# add testmode, generalise?
         # fetch neuron-neuron connectivity for only the filtered neurons within PRIMARY rois only and collapse rois to total
         neuron_df, conn_df = fetch_adjacencies(sources=accessions, targets=accessions)
         conn_df = conn_df.groupby(['bodyId_pre', 'bodyId_post'], as_index=False)['weight'].sum()
         return conn_df#add to stack? in wrapper
 
-    def generate_template(self, dataset):
-        vfb_ids = self.vc.neo_query_wrapper.xref_2_vfb_id(db=dataset).items()#dictionary comprehension, key first pos (accession) value VFB_id
+    def generate_template(self, dataset, conn_df):
+        vfb_ids = self.vc.neo_query_wrapper.xref_2_vfb_id(db=dataset).items()
+        vfb_ids = {k: v[0]['vfb_id'] for (k, v) in vfb_ids}
+        conn_df = conn_df.applymap(str)
+        conn_df['bodyId_pre']=conn_df['bodyId_pre'].map(vfb_ids)
+        conn_df['bodyId_post']=conn_df['bodyId_post'].map(vfb_ids)
+        conn_df.rename(columns={'bodyId_pre': 'ID', 'bodyId_post': 'FACT', 'weight': 'Weight'}, inplace=True)
+        conn_df['ID']=conn_df['ID'].str.replace('_', ':')
+        conn_df['FACT']=conn_df['FACT'].str.replace('_', ':')
+        return conn_df
 
 
 
-#pull from vfb connect accessions > then to neuprint or catmaid depending, match catmaid to neuprint (neuprint or catmaid arg)/ CATMIAD runner, neuprint runner.
-#args for both should stay the same
+#TODO pull from vfb connect accessions > then to neuprint or catmaid depending, match catmaid to neuprint (neuprint or catmaid arg)/ CATMIAD runner, neuprint runner.
+#TODO args for both should stay the same
 
-vc=VfbConnect()
-vfb_ids = vc.neo_query_wrapper.xref_2_vfb_id(db='neuprint_JRC_Hemibrain_1point1')
-vfb_ids = {k:v[0]['vfb_id'] for (k,v) in vfb_ids.items()}
-conn_df = conn_df.applymap(str)
-conn_df['bodyId_pre']=conn_df['bodyId_pre'].map(vfb_ids)
-conn_df['bodyId_post']=conn_df['bodyId_post'].map(vfb_ids)
-conn_df.rename(columns={'bodyId_pre': 'ID', 'bodyId_post': 'FACT', 'weight': 'Weight'}, inplace=True)
-conn_df['ID']=conn_df['ID'].str.replace('_', ':')
-conn_df['FACT']=conn_df['FACT'].str.replace('_', ':')
-robot_template_df=pd.DataFrame({'ID':['ID'], 'FACT':["I 'synapsed to' %"], 'Weight':['^A n2o:weight']})
-
-conn_df.to_csv('Robot_template.tsv', sep='\t', index=False)
-
-#pull from neuprint function
-#catmaid/neuprint
 
--- src/VFB_connectomics_import/script_runner.py
+++ src/VFB_connectomics_import/script_runner.py
@@ -1,18 +1,16 @@
 from connectomics_import import ConnectomicsImport
 
-#could add argparse here
-
-#will be from vfb in connectomics_import
-
-nc=neuprint.Client('https://neuprint.janelia.org',
-                      dataset='hemibrain:v1.1',
-                      token=''
-                   
-accessions = list(nc.fetch_custom("""MATCH (n:hemibrain_Neuron) WHERE exists(n.type) RETURN n.bodyId as ID""")['ID'])
+#TODO add argparse here
 
 ci=ConnectomicsImport(neuprint_endpoint='https://neuprint.janelia.org',
                       neuprint_dataset='hemibrain:v1.1',
-                      neuprint_token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbTcxQGNhbS5hYy51ayIsImxldmVsIjoibm9hdXRoIiwiaW1hZ2UtdXJsIjoiaHR0cHM6Ly9saDQuZ29vZ2xldXNlcmNvbnRlbnQuY29tLy1pNDJ6MjMtUkUxTS9BQUFBQUFBQUFBSS9BQUFBQUFBQUFBQS9BTVp1dWNua2hPVm9rZExRVURESkdNdHVSXzJadTROeUNnL3M5Ni1jL3Bob3RvLmpwZz9zej01MD9zej01MCIsImV4cCI6MTc4NjEyODQzMn0.nqLX9LffweDHZ5QMqhSodH7w35fRO7gXAvINYE-3OGg')
+                      neuprint_token='')
 
-conn_df=ci.get_adjacencies(accessions=accessions)
+accessions=ci.get_accessions_from_vfb('neuprint_JRC_Hemibrain_1point1')
+
+conn_df=ci.get_adjacencies_neuprint(accessions=accessions[0:100])
+
+robot_template_df=ci.generate_template('neuprint_JRC_Hemibrain_1point1', conn_df)
+
+robot_template_df.to_csv('Robot_template.tsv', sep='\t', index=False)
 
